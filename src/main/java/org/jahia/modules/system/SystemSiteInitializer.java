acacde29bdea086f471b062f02439036a3e33a7e
cedmail <cmailleux@jahia.com>
2017-05-03 11:16:52 -0400

    QA-9521 Check if we are processing server before updating system site node

--- src/main/java/org/jahia/modules/system/SystemSiteInitializer.java
+++ src/main/java/org/jahia/modules/system/SystemSiteInitializer.java
@@ -48,6 +48,7 @@ import org.jahia.services.content.JCRSessionWrapper;
 import org.jahia.services.content.JCRTemplate;
 import org.jahia.services.content.decorator.JCRSiteNode;
 import org.jahia.services.sites.JahiaSitesService;
+import org.jahia.settings.SettingsBean;
 import org.springframework.beans.factory.InitializingBean;
 
 import javax.jcr.NodeIterator;
@@ -57,6 +58,8 @@ import javax.jcr.query.Query;
 public class SystemSiteInitializer implements InitializingBean {
     private JCRTemplate jcrTemplate;
     private JahiaSitesService sitesService;
+    private SettingsBean settingsBean;
+
 
     public void setJcrTemplate(JCRTemplate jcrTemplate) {
         this.jcrTemplate = jcrTemplate;
@@ -66,25 +69,30 @@ public class SystemSiteInitializer implements InitializingBean {
         this.sitesService = sitesService;
     }
 
+    public void setSettingsBean(SettingsBean settingsBean) {
+        this.settingsBean = settingsBean;
+    }
     @Override
     public void afterPropertiesSet() throws Exception {
-        jcrTemplate.doExecuteWithSystemSession(new JCRCallback<Object>() {
-            @Override
-            public Object doInJCR(JCRSessionWrapper session) throws RepositoryException {
-                Query q = session.getWorkspace().getQueryManager().createQuery("select * from [jnt:virtualsite]", Query.JCR_SQL2);
-                NodeIterator ni = q.execute().getNodes();
+        if (settingsBean.isProcessingServer()) {
+            jcrTemplate.doExecuteWithSystemSession(new JCRCallback<Object>() {
+                @Override
+                public Object doInJCR(JCRSessionWrapper session) throws RepositoryException {
+                    Query q = session.getWorkspace().getQueryManager().createQuery("select * from [jnt:virtualsite]", Query.JCR_SQL2);
+                    NodeIterator ni = q.execute().getNodes();
 
-                while (ni.hasNext()) {
-                    JCRSiteNode node = (JCRSiteNode) ni.next();
-                    if (!node.getName().equals(JahiaSitesService.SYSTEM_SITE_KEY) && node.hasProperty("j:languages")) {
-                        sitesService.updateSystemSiteLanguages(node, session);
+                    while (ni.hasNext()) {
+                        JCRSiteNode node = (JCRSiteNode) ni.next();
+                        if (!node.getName().equals(JahiaSitesService.SYSTEM_SITE_KEY) && node.hasProperty("j:languages")) {
+                            sitesService.updateSystemSiteLanguages(node, session);
+                        }
                     }
+                    JCRSiteNode siteByKey = sitesService.getSiteByKey(JahiaSitesService.SYSTEM_SITE_KEY, session);
+                    sitesService.updateSystemSitePermissions(siteByKey, session);
+                    session.save();
+                    return null;
                 }
-                JCRSiteNode siteByKey = sitesService.getSiteByKey(JahiaSitesService.SYSTEM_SITE_KEY, session);
-                sitesService.updateSystemSitePermissions(siteByKey, session);
-                session.save();
-                return null;
-            }
-        });
+            });
+        }
     }
 }
